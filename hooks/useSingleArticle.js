dd29d3563ff8748aa493663267972a2661a7367c
SukunDev <sukundev32@gmail.com>
2024-04-16 02:17:56 +0700

    Master

--- hooks/useSingleArticle.js
+++ hooks/useSingleArticle.js
@@ -1,5 +1,7 @@
 import { getTableContent } from "@/utils/heper";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import hljs from "highlight.js";
+import "highlight.js/styles/github-dark.css";
 
 function useSingleArticle({ post }) {
   const tableContent = getTableContent(`<div>${post.content}</div>`);
@@ -8,6 +10,12 @@ function useSingleArticle({ post }) {
   const handleTableOfContent = () => {
     setTableOfContentStatus(!tableOfContentStatus);
   };
+
+  useEffect(() => {
+    if (window !== undefined) {
+      hljs.highlightAll();
+    }
+  });
 
   return {
     handleTableOfContent,
