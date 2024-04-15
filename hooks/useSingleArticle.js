2f09a42f9c1840962f162c306eacc711860307a7
SukunDev <sukundev32@gmail.com>
2024-04-15 19:58:17 +0700

    Master

--- hooks/useSingleArticle.js
+++ hooks/useSingleArticle.js
@@ -1,26 +1,13 @@
 import { getTableContent } from "@/utils/heper";
-import React, { useEffect, useState } from "react";
-import hljs from "highlight.js";
-import "highlight.js/styles/github-dark.css";
+import React, { useState } from "react";
 
 function useSingleArticle({ post }) {
   const tableContent = getTableContent(`<div>${post.content}</div>`);
   const [tableOfContentStatus, setTableOfContentStatus] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleTableOfContent = () => {
     setTableOfContentStatus(!tableOfContentStatus);
   };
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
-  useEffect(() => {
-    if (isLoading) {
-      hljs.highlightAll();
-    }
-  }, [isLoading]);
 
   return {
     handleTableOfContent,
